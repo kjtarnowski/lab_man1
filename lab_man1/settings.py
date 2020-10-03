8fca09bb5b01279e5f1906bdded324593dc661d8
krzysiek <kj.tarnowski@gmail.com>
2020-10-03 12:50:46 +0200

    postgresql configured

--- lab_man1/settings.py
+++ lab_man1/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'lab_man1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lab_man1',
+        'USER': 'lab_man1',
+        'PASSWORD': 'django111',
+        'HOST': 'localhost'
     }
 }
 
