32e124e7de794b68b34b17e02e0e7084df7fa621
krzysiek <kj.tarnowski@gmail.com>
2020-12-03 16:45:57 +0100

    api_views corrected

--- experiments/api_views.py
+++ experiments/api_views.py
@@ -1,5 +1,4 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
 
 from experiments.models import Compound, Experiment, Project, Aparat, ExperimentType, ExperimentalSet, LabPerson
 from experiments.serializers import (
@@ -16,40 +15,35 @@ from experiments.serializers import (
 class LabPersonViewset(viewsets.ModelViewSet):
     queryset = LabPerson.objects.all()
     serializer_class = LabPersonSerializer
-    permission_classes = [IsAuthenticated]
 
 
 class CompoundViewset(viewsets.ModelViewSet):
     queryset = Compound.objects.all()
     serializer_class = CompoundSerializer
-    permission_classes = [IsAuthenticated]
 
 
 class ExperimentViewset(viewsets.ModelViewSet):
     queryset = Experiment.objects.all()
     serializer_class = ExperimentSerializer
-    permission_classes = [IsAuthenticated]
 
 
 class ProjectViewset(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
-    permission_classes = [IsAuthenticated]
+
 
 
 class AparatViewset(viewsets.ModelViewSet):
     queryset = Aparat.objects.all()
     serializer_class = AparatSerializer
-    permission_classes = [IsAuthenticated]
 
 
 class ExperimentTypeViewset(viewsets.ModelViewSet):
     queryset = ExperimentType.objects.all()
     serializer_class = ExperimentTypeSerializer
-    permission_classes = [IsAuthenticated]
 
 
 class ExperimentalSetViewset(viewsets.ModelViewSet):
     queryset = ExperimentalSet.objects.all()
     serializer_class = ExperimentalSetSerializer
-    permission_classes = [IsAuthenticated]
+
--- lab_man1/settings.py
+++ lab_man1/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    'oauth2_provider',
     'rest_framework.authtoken',
     'rest_auth',
     "rest_framework_swagger",
